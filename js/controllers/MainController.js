df8cb6175dd7c26a925a8cb79c187a4ae3912e99
Julia Noedtvedt <julia@myzine.no>
2016-04-02 19:54:11 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	css/main.css

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,6 +1,7 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.posts = [
-  	{ 
+  	{
+      id: 1, 
     	user: 'Mags Yip',
       rating: 235, 
     	role: 'Editor',
@@ -13,6 +14,7 @@ app.controller('MainController', ['$scope', function($scope) {
     	saves: 0
   	},
     {
+      id: 2,
       user: 'Ooh Couture',
       rating: 235, 
       role: 'Blogger',
@@ -22,7 +24,8 @@ app.controller('MainController', ['$scope', function($scope) {
       url: "../ciara-holiday-style.html",
       saves:  0
     }, 
-  	{ 
+  	{
+      id: 3, 
     	user: 'Julia Dahl',
       rating: 235,  
     	role: 'Editor',
@@ -34,7 +37,8 @@ app.controller('MainController', ['$scope', function($scope) {
       url: "../ciara-holiday-style.html",
     	saves: 0
   	}, 
-  	{ 
+  	{
+      id: 4, 
     	User: 'Alexandra Dean',
       rating: 235,  
     	role: 'Editor',
@@ -46,7 +50,8 @@ app.controller('MainController', ['$scope', function($scope) {
       url: "../ciara-holiday-style.html",
     	saves: 0,
   	}, 
-  	{ 
+  	{
+      id: 5, 
     	user: 'Mags Yip',
       rating: 235,  
     	role: 'Editor',
@@ -58,7 +63,8 @@ app.controller('MainController', ['$scope', function($scope) {
       url: "../ciara-holiday-style.html",
     	saves: 0,
   	},
-    { 
+    {
+      id: 6, 
       user: 'Mags Yip',
       rating: 235,  
       role: 'Editor',
@@ -70,7 +76,8 @@ app.controller('MainController', ['$scope', function($scope) {
       url: "../ciara-holiday-style.html",
       saves: 0,
     },
-    { 
+    {
+      id: 7, 
       user: 'Mags Yip',
       rating: 235,  
       role: 'Editor',
@@ -82,7 +89,8 @@ app.controller('MainController', ['$scope', function($scope) {
       url: "../ciara-holiday-style.html",
       saves: 0,
     }, 
-    { 
+    {
+      id: 8, 
       user: 'Julia Dahl',
       rating: 235,  
       role: 'Editor',
@@ -94,7 +102,8 @@ app.controller('MainController', ['$scope', function($scope) {
       url: "../ciara-holiday-style.html",
       saves: 0,
     }, 
-    { 
+    {
+      id: 9, 
       User: 'Alexandra Dean',
       rating: 235,  
       role: 'Editor',
@@ -106,7 +115,8 @@ app.controller('MainController', ['$scope', function($scope) {
       url: "../ciara-holiday-style.html",
       saves: 0,
     }, 
-    { 
+    {
+      id: 10, 
       user: 'Mags Yip',
       rating: 235,  
       role: 'Editor',
@@ -118,7 +128,8 @@ app.controller('MainController', ['$scope', function($scope) {
       url: "../ciara-holiday-style.html",
       saves: 0,
     },
-    { 
+    {
+      id: 11, 
       user: 'Mags Yip',
       rating: 235,  
       role: 'Editor',
@@ -129,7 +140,48 @@ app.controller('MainController', ['$scope', function($scope) {
       saves: 0,
     }
   ];
+  $scope.lists = [
+    {name: 'My wish list'},
+    {name: 'New dress'},
+    {name: 'Sneakers'},
+    {name: 'Read later'},
+    {name: 'Jeans'},
+  ];
+  $scope.activePost = null;
+  $scope.addListMode = null;
   $scope.plusOne = function(index) { 
   	$scope.posts[index].saves += 1; 
 	};
+  $scope.saveActiveTo = function(list){
+    list.savedItems = list.savedItems || [];
+    var index = list.savedItems.indexOf($scope.activePost);
+    if (index >= 0){
+      list.savedItems.splice(index, 1);
+    }else{
+      list.savedItems.push($scope.activePost);
+    }
+    setTimeout(function(){
+      $scope.savePost(null);
+      $scope.$apply();
+    }, 250);
+  };
+  $scope.overlayClass = function(postId){
+    return postId == $scope.activePost ? 'shown' : '';
+  }
+  $scope.listClass = function(list){
+    return (list.savedItems || []).indexOf($scope.activePost) >= 0 ? 'saved' : '';
+  }
+  $scope.savePost = function(postId){
+    $scope.activePost = postId;
+  }
+  $scope.addList = function(newListName){
+    $scope.lists.push({name: newListName});
+    $scope.setMode('');
+  }
+  $scope.setMode = function(mode){
+    $scope.addListMode = mode;
+  }
+  $scope.getMode = function(){
+    return $scope.addListMode;
+  }
 }]);
