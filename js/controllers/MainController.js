e58d60ed5a658488dc4029638d79bb29b4b95d96
Ian Robson <ian@byian.co.uk>
2016-04-05 19:09:37 +0100

    home page filter

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -10,7 +10,10 @@ app.controller('MainController', ['$scope', function($scope) {
     	cover: "../images/feed_1.png",
       category: 'Looks for less',
       title: "Get Ciara's casual holiday style",
-      url: "../article.html", 
+      url: "../article.html",
+      trends: ['Looks for less'],
+      colours: [],
+      products: [],
     	saves: 0
   	},
     {
@@ -335,6 +338,98 @@ app.controller('MainController', ['$scope', function($scope) {
       saves: 0,
     }
   ];
+  for (var i=1;i<=10;i++){
+    $scope.posts.push({
+      id: 100 + i, 
+      user: 'Mags Yip',
+      rating: 235,  
+      role: 'Editor',
+      type: 'flatlay',  
+      pubdate: new Date('2011', '08', '16'), 
+      cover: "images/all-jeans/jeans view all "+i+".jpg",
+      url: "../lookpage.html",
+      products: ['jeans'],
+      saves: 0,
+    });
+  }
+  for (var i=1;i<=7;i++){
+    $scope.posts.push({
+      id: 200 + i, 
+      user: 'Mags Yip',
+      rating: 235,  
+      role: 'Editor',
+      type: 'flatlay',  
+      pubdate: new Date('2011', '08', '16'), 
+      cover: "images/black-jeans/black jeans "+i+".jpg",
+      url: "../lookpage.html",
+      products: ['jeans'],
+      colours: ['black'],
+      saves: 0,
+    });
+  }
+  for (var i=1;i<=14;i++){
+    $scope.posts.push({
+      id: 200 + i, 
+      user: 'Mags Yip',
+      rating: 235,  
+      role: 'Editor',
+      type: 'flatlay',  
+      pubdate: new Date('2011', '08', '16'), 
+      cover: "images/ripped-jeans/Cat ripped jeans "+i+".jpg",
+      url: "../lookpage.html",
+      products: ['jeans'],
+      trends: ['ripped jeans'],
+      saves: 0,
+    });
+  }
+  $scope.filterCategories = {
+    trends: ['most popular', 'trending' ,'new in', 'ponchos', 'stripes', 'casual', 'classic shorts', 'ripped jeans', 'on your head', 'fringe fever',
+'girls night out', 'chic to work', 'animal print', 'flared pants', 'platforms', 'flower power', 'tartan', 'pretty pastel', 'aztec invasion'],
+    colours: ['brown','red','orange','yellow','green','darkgreen','black','grey','purple','blue','light blue','nude'],
+    products: ['jacket','jeans','shoes','trousers']
+  }
+  /*
+  $scope.filterCategories = {
+    trends: _.filter(_.uniq(_.flatten(_.pluck($scope.posts, "trends"))), function(s){return !!s;}),
+    colours: _.filter(_.uniq(_.flatten(_.pluck($scope.posts, "colours"))), function(s){return !!s;}),
+    products: _.filter(_.uniq(_.flatten(_.pluck($scope.posts, "products"))), function(s){return !!s;})
+  }
+  */
+  $scope.getBgColour = function(c){
+    return {
+      'nude': '#e5b292'
+    }[c] || c;
+  }
+  $scope.getActiveClass = function(a){
+    return a ? 'active' : '';
+  }
+  $scope.toggle = function(a,b){
+    return a == b ? '' : b;
+  }
+  $scope.filters = {
+    trends: null,
+    colours: null,
+    products: null
+  };
+  $scope.filter = function(type, by){
+    if (type){
+      $scope.filters[type] = by;
+    }
+    $scope.filteredPosts = _.filter($scope.posts, function(post){
+      for (var f in $scope.filters){
+        if ($scope.filters[f] && (!post[f] || post[f].indexOf($scope.filters[f]) < 0 )){
+          return false;
+        }
+      }
+      return true;
+    });
+
+    setTimeout(function(){
+      window.dispatchEvent(new Event('resize'));
+    }, 10);
+  }
+  $scope.filter();
+
   $scope.lists = [
     {name: 'My wish list'},
     {name: 'New dress'},
